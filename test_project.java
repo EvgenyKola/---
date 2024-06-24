3c67a8dcc28b3f32dba14475f63f3e466ae1f166
EvgenyKola <korzenokev@gmail.com>
2024-06-25 00:03:57 +0300

    test_project.java

--- test_project.java
+++ test_project.java
@@ -7,39 +7,28 @@ public class test_project {
     public static String mathSymbols = "+-*/";
 
     public static void main (String[] args) {
-       
         System.out.println("Введите выражение");
         Scanner keyboardScan = new Scanner (System.in);
         String string = keyboardScan.nextLine();
-        
         int o = 0; 
-      
         Boolean lat = false;
         Boolean rom = false;
         Boolean contune = true;
-    
         String romString = "ivxzlcdm";
-        
         String [] calcArray = new String [string.length()];
-
         Arrays.fill(calcArray, "");
  
-
-    for (String s: string.split("")) {
-
+    for (String s: string.toLowerCase().split("")) {
         try {
             int i = Integer.parseInt (s);
             lat = true;
             calcArray [o] += s;
         }
-        
         catch (NumberFormatException e) {
-            
             if (romString.contains(s)){
                 calcArray [o] += s;
                 rom = true;     
                 }
-
             else if (!mathSymbols.contains(s)){
                 System.out.println("Недопустимый символ");
                 break;
@@ -51,33 +40,27 @@ public class test_project {
                 o++;
                 }
         }
-
         if (rom == lat) {
             System.out.println("Ошибка ввода данных");
             contune = false;
             break;
         }
     }
-
         if (contune == true & lat == true) {
             latCalc (calcArray, o, false);
         }
         else if (contune == true & rom == true) {
             romToLat (calcArray, o);
         }
-    
     }
 
     public static void romToLat (String[] args, int arg) {
-
         int currentElement = 0;
         int beforeElement = 0;
         int calcElement = 0;
         String result = "";
         String [] calcArray = new String [arg+1];
-
         HashMap<String, Integer> latRomMap = new HashMap<>();
-
         latRomMap.put("i", 1);
         latRomMap.put("v", 5);
         latRomMap.put("x", 10);
@@ -92,43 +75,48 @@ public class test_project {
                     if (calcElement == 0) {
                         calcElement = currentElement = beforeElement = latRomMap.get(element);
                     }
-
                     else {
-
                         currentElement = latRomMap.get(element);
-                        
                         if (beforeElement >= currentElement) {
                             calcElement += currentElement;
                             beforeElement = currentElement;
                         }
-
                         else {
                             calcElement += currentElement - (beforeElement*2);
                             beforeElement = currentElement;
                         }
                     }   
                 }
-
                 else {
                     currentElement=calcElement=beforeElement=0;
                     result = element;
                 }
             }
-
             if (calcElement != 0) {result = String.valueOf(calcElement);}
-
             calcArray[i] = result;
-
         }
         latCalc (calcArray, arg, true);
     }
 
-
     public static void latToRom (int arg) {
-        System.out.print("Результат (Перевод): " + arg);
+        String [] rom = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+        String result = "";
+        int [] lat = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        int i = 0;
+
+        while (i < rom.length) {
+            while (arg >= lat[i]) {
+                int t = arg/lat[i];
+                arg = arg%lat[i];
+                for(int l=0; l<t; l++) {
+                    result += rom[i];
+                }
+            }
+            i++;
+        }
+        System.out.println("Результат Rom:  "+result);
     }
-
-
+ 
     public static void latCalc (String[] args, int arg, Boolean rom) {
 
         //ScriptEngine не используется специально
@@ -137,7 +125,6 @@ public class test_project {
         int n=0;
     
         for (int i = 0; i <= arg; i++) {
-
             try {
                 if (i == 0 ) {
                     n = Integer.parseInt (args[i]);
@@ -157,20 +144,12 @@ public class test_project {
                     }
                 }
             }
-            
             catch (NumberFormatException e) {
                 System.out.print("Ошибка");
                 break;
             }
         }
-
-        if (rom) {
-            latToRom(result);
-        }
-
-        else {
-            System.out.print("Результат: " + result);
-        }
+        if (rom) {latToRom(result);}
+        else {System.out.print("Результат: " + result);}
     }
-
 }
